baca571ee3c965ccd8483acd4ae2a22ff52d687d
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-09-01 21:25:55 -0400

    Use 0/0 for private subnet and 10/8 for public subnet routing

--- main.tf
+++ main.tf
@@ -95,12 +95,26 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "region1" {
   }
 }
 
-# Add 10/8 route to region1 VPC route tables
-resource "aws_route" "region1" {
-  for_each = toset(flatten([for k,v in var.cloud_vpcs_region1 : concat(module.cloud_vpc_region1[k].public_route_table_ids,module.cloud_vpc_region1[k].private_route_table_ids)]))
+
+# Add 10/8 route to region1 VPC private route tables
+resource "aws_route" "region1_public" {
+  for_each = toset(flatten([for k,v in var.cloud_vpcs_region1 : concat(module.cloud_vpc_region1[k].public_route_table_ids)]))
 
   route_table_id         = each.key
   destination_cidr_block = "10.0.0.0/8"
+  transit_gateway_id             = aws_ec2_transit_gateway.tgw_region1.id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+# Add 0/0 route to region1 VPC private route tables
+resource "aws_route" "region1_private" {
+  for_each = toset(flatten([for k,v in var.cloud_vpcs_region1 : concat(module.cloud_vpc_region1[k].private_route_table_ids)]))
+
+  route_table_id         = each.key
+  destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id             = aws_ec2_transit_gateway.tgw_region1.id
 
   timeouts {
@@ -224,11 +238,19 @@ module "region1_onprem_test_ec2" {
   vpc_id = module.onprem_vpc_region1.vpc_id
 }
 
-# # Add Cloud Routes to OnPrem route tables, point to the StrongWAN gateway
-
+# # Add 10/8 to Region1 OnPrem Public route tables, point to the StrongWAN gateway
 resource "aws_route" "region1_public_vpn_gw" {
-  for_each = toset(flatten(concat(module.onprem_vpc_region1.public_route_table_ids,module.onprem_vpc_region1.private_route_table_ids)))
+  for_each = toset(module.onprem_vpc_region1.public_route_table_ids)
   route_table_id         = each.key
   destination_cidr_block = "10.0.0.0/8"
+  network_interface_id = aws_cloudformation_stack.region_1vpn_gateway.outputs.NicID
+}
+
+
+# # Add 0/0 to Region1 OnPrem private route tables, point to the StrongWAN gateway
+resource "aws_route" "region1_private_vpn_gw" {
+  for_each = toset(module.onprem_vpc_region1.private_route_table_ids)
+  route_table_id         = each.key
+  destination_cidr_block = "0.0.0.0/0"
   network_interface_id = aws_cloudformation_stack.region_1vpn_gateway.outputs.NicID
 }
