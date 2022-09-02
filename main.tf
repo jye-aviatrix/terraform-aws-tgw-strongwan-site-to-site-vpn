e5314a7654dd45a6c83635a52a1c95f976b2c9cb
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-09-02 11:28:50 -0400

    Add TGW route to enable cross region communication

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "tgw_peering" {
   transit_gateway_id      = aws_ec2_transit_gateway.tgw_region1.id
 
   tags = {
-    Name = "TGW Peering Requestor"
+    Name = "region1-to-region2-peering"
   }
 }
 
@@ -17,7 +17,35 @@ resource "aws_ec2_transit_gateway_peering_attachment_accepter" "tgw_peering" {
   transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
 
   tags = {
-    Name = "Example cross-account attachment"
+    Name = "region2-to-region1-peering"
   }
+  provider = aws.secondary
+}
+
+# TGW peering doesn't propagate route across. Need static route in TGW.
+# CloudWAN is supposed to overcome this limit.
+resource "aws_ec2_transit_gateway_route" "region1_to_region2_cloud" {
+  destination_cidr_block         = "10.200.0.0/16"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw_region1.association_default_route_table_id
+}
+
+resource "aws_ec2_transit_gateway_route" "region1_to_region2_onprem" {
+  destination_cidr_block         = "10.20.0.0/16"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw_region1.association_default_route_table_id
+}
+
+resource "aws_ec2_transit_gateway_route" "region2_to_region1_cloud" {
+  destination_cidr_block         = "10.100.0.0/16"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment_accepter.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw_region2.association_default_route_table_id
+  provider = aws.secondary
+}
+
+resource "aws_ec2_transit_gateway_route" "region2_to_region1_onprem" {
+  destination_cidr_block         = "10.10.0.0/16"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment_accepter.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw_region2.association_default_route_table_id
   provider = aws.secondary
 }
